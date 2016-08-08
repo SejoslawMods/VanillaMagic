1dfef671a08f54778136188dcaa349e94f566e43
Sejoslaw <k.dobrzynski94@gmail.com>
2016-08-08 22:52:09 +0200

    Bumped version to 0.11.0.0

--- VanillaMagic/src/main/java/seia/vanillamagic/VanillaMagic.java
+++ VanillaMagic/src/main/java/seia/vanillamagic/VanillaMagic.java
@@ -18,7 +18,7 @@ import seia.vanillamagic.quest.QuestList;
 public class VanillaMagic 
 {
 	public static final String MODID = "vanillamagic";
-	public static final String VERSION = "1.10.2-0.10.2.2";
+	public static final String VERSION = "1.10.2-0.11.0.0";
 	public static final String NAME = "Vanilla Magic";
 	
 	@Mod.Instance
