84f9690155c14be82e78fa668335528ee72577cc
Sejoslaw <k.dobrzynski94@gmail.com>
2016-08-26 19:19:49 +0200

    Ported to 0.13.2.0. Fixed some hostile mobs requirements.

--- VanillaMagic/src/main/java/seia/vanillamagic/utils/spell/EnumSpellHelper.java
+++ VanillaMagic/src/main/java/seia/vanillamagic/utils/spell/EnumSpellHelper.java
@@ -3,5 +3,6 @@ package seia.vanillamagic.utils.spell;
 public class EnumSpellHelper 
 {
 	public static final EnumWand SUMMON_REQUIRED_WAND = EnumWand.NETHER_STAR;
-	public static final int SUMMON_COST_ITEMSTACK = 32;
+	public static final int SUMMON_FRIENDLY_COST_ITEMSTACK = 32;
+	public static final int SUMMON_HOSTILE_COST_ITEMSTACK = 8;
 }
