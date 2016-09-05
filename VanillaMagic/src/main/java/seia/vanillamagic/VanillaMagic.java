0ff73cd2b2564fa763038dd7ca0206c37c3db5eb
Sejoslaw <k.dobrzynski94@gmail.com>
2016-09-05 16:39:03 +0200

    More quest JSON fixing.

--- VanillaMagic/src/main/java/seia/vanillamagic/VanillaMagic.java
+++ VanillaMagic/src/main/java/seia/vanillamagic/VanillaMagic.java
@@ -75,7 +75,7 @@ public class VanillaMagic
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
-		//BookRegistry.INSTANCE.postInit(); // TODO:
+		BookRegistry.INSTANCE.postInit();
 		CustomTileEntityHandler.INSTANCE.postInit();
 		EnchantedBucketHelper.registerFluids();
 		PotionedCrystalHelper.registerRecipes();
