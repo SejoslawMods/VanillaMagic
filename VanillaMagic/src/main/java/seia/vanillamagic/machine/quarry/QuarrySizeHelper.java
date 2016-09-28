e3d5b98469dd1c2aa296f1718910f028fc96cf11
Sejoslaw <k.dobrzynski94@gmail.com>
2016-09-28 10:39:16 +0200

    Removed old Quarry size.

--- VanillaMagic/src/main/java/seia/vanillamagic/machine/quarry/QuarrySizeHelper.java
+++ VanillaMagic/src/main/java/seia/vanillamagic/machine/quarry/QuarrySizeHelper.java
@@ -3,13 +3,6 @@ package seia.vanillamagic.machine.quarry;
 public class QuarrySizeHelper 
 {
 	/**
-	 * It's (size)x(size) but (size-2)x(size-2) is for digging <br>
-	 * ChunkNumber * BlocksInChunk
-	 */
-	@Deprecated
-	public static final int BASIC_QUARRY_SIZE = 8 * 16;
-	
-	/**
 	 * Chunk size in blocks.
 	 */
 	public static final int CHUNK_SIZE = 16;
