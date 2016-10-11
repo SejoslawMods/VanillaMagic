458c68d9d7803acd51e014c2fc6db056512cb425
Sejoslaw <k.dobrzynski94@gmail.com>
2016-10-11 13:26:11 +0200

    Cleaned TileChunkLoader.

--- VanillaMagic/src/main/java/seia/vanillamagic/tileentity/chunkloader/TileChunkLoader.java
+++ VanillaMagic/src/main/java/seia/vanillamagic/tileentity/chunkloader/TileChunkLoader.java
@@ -11,43 +11,6 @@ public class TileChunkLoader extends CustomTileEntity implements IChunkLoader
 {
 	public static final String REGISTRY_NAME = TileChunkLoader.class.getSimpleName();
 	
-//	public void validate() 
-//	{
-//		super.validate();
-//		if ((!this.worldObj.isRemote) && (this.chunkTicket == null)) 
-//		{
-//			Ticket ticket = ForgeChunkManager.requestTicket(VanillaMagic.INSTANCE, this.worldObj, ForgeChunkManager.Type.NORMAL);
-//			if (ticket != null) 
-//			{
-//				forceChunkLoading(ticket);
-//			}
-//		}
-//	}
-	
-//	public void invalidate() 
-//	{
-//		super.invalidate();
-//		stopChunkLoading();
-//	}
-	/*
-	public void unforceChunkLoading() 
-	{
-		for(Object obj : this.chunkTicket.getChunkList()) 
-		{
-			ChunkPos coord = (ChunkPos) obj;
-			ForgeChunkManager.unforceChunk(this.chunkTicket, coord);
-		}
-	}
-	*/
-//	public void stopChunkLoading() 
-//	{
-//		if (this.chunkTicket != null) 
-//		{
-//			ForgeChunkManager.releaseTicket(this.chunkTicket);
-//			this.chunkTicket = null;
-//		}
-//	}
-	
 	public void update() 
 	{
 		if(!QuestChunkLoader.isChunkLoaderBuildCorrectly(worldObj, this.pos))
