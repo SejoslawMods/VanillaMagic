45fd397867b1ffc8c90a03dc897b5d32dd77ecd4
Krzysztof <krzysztof@studiovector.pl>
2019-12-08 14:51:21 +0100

    Fixed config

--- src/main/java/com/github/sejoslaw/vanillamagic/common/config/VMConfigAchievements.java
+++ src/main/java/com/github/sejoslaw/vanillamagic/common/config/VMConfigAchievements.java
@@ -1,147 +1,147 @@
-package com.github.sejoslaw.vanillamagic.config;
+package com.github.sejoslaw.vanillamagic.common.config;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-
-import org.apache.logging.log4j.Level;
-
+import com.github.sejoslaw.vanillamagic.api.quest.IQuest;
+import com.github.sejoslaw.vanillamagic.core.VMLogger;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.apache.logging.log4j.Level;
 
-import com.github.sejoslaw.vanillamagic.api.quest.IQuest;
-import com.github.sejoslaw.vanillamagic.core.VanillaMagic;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Class which operates on additional VM configuration files.
- * 
+ *
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
 public class VMConfigAchievements {
-	public static final String VM_DIRECTORY = "/VanillaMagic/";
+    public static final String VM_DIRECTORY = "/VanillaMagic/";
 
-	private final String achievements = "Quests.json";
-	private final String aboutAchievements = "About Quests.txt";
+    private final String quests = "Quests.json";
+    private final String aboutQuests = "About Quests.txt";
 
-	private File modConfigurationDirectory;
-	private File modFile;
-	private File fileAchievements;
-	private File fileAboutAchievements;
+    private File modConfigurationDirectory;
+    private File modFile;
+    private File fileQuests;
+    private File fileAboutQuests;
 
-	/**
-	 * modConfigurationDirectory - config/VanillaMagic/ <br>
-	 * modFile - vanillamagic-[].jar OR /bin/ (if modding)
-	 */
-	public VMConfigAchievements(File modConfigurationDirectory, File modFile) {
-		this.modConfigurationDirectory = modConfigurationDirectory;
-		this.modFile = modFile;
+    /**
+     * modConfigurationDirectory - config/VanillaMagic/ <br>
+     * modFile - vanillamagic-[].jar OR /bin/ (if modding)
+     */
+    public VMConfigAchievements(File modConfigurationDirectory, File modFile) {
+        this.modConfigurationDirectory = modConfigurationDirectory;
+        this.modFile = modFile;
 
-		if (!this.modConfigurationDirectory.exists()) {
-			this.modConfigurationDirectory.mkdirs();
-			VanillaMagic.logInfo("VanillaMagic config directory created.");
-		}
+        if (!this.modConfigurationDirectory.exists()) {
+            this.modConfigurationDirectory.mkdirs();
+            VMLogger.logInfo("VanillaMagic config directory created.");
+        }
 
-		VanillaMagic.logInfo("VanillaMagic config directory loaded.");
-		this.fileAchievements = unzip(this.achievements, fileAchievements);
-		this.fileAboutAchievements = unzip(this.aboutAchievements, fileAboutAchievements);
-		readAchievements();
-	}
+        VMLogger.logInfo("VanillaMagic config directory loaded.");
+        this.fileQuests = unzip(this.quests, fileQuests);
+        this.fileAboutQuests = unzip(this.aboutQuests, fileAboutQuests);
 
-	/**
-	 * Unzip the specified file from JAR - copy into directory from inside of JAR
-	 * file.
-	 */
-	@SuppressWarnings("resource")
-	public File unzip(String fileName, File localFile) {
-		localFile = new File(this.modConfigurationDirectory, fileName);
+        readQuests();
+    }
 
-		if (!localFile.exists()) {
-			try {
-				localFile.createNewFile();
+    /**
+     * Unzip the specified file from JAR - copy into directory from inside of JAR
+     * file.
+     */
+    @SuppressWarnings("resource")
+    public File unzip(String fileName, File localFile) {
+        localFile = new File(this.modConfigurationDirectory, fileName);
 
-				if (!this.modFile.isDirectory()) {
-					JarFile jarFile = new JarFile(this.modFile);
-					Enumeration<JarEntry> enumeration = jarFile.entries();
+        if (localFile.exists()) {
+            return localFile;
+        }
 
-					while (enumeration.hasMoreElements()) {
-						JarEntry file = (JarEntry) enumeration.nextElement();
+        try {
+            localFile.createNewFile();
 
-						if (file.getName().equals(fileName)) {
-							InputStream is = jarFile.getInputStream(file);
-							FileOutputStream fos = new FileOutputStream(localFile);
+            if (!this.modFile.isDirectory()) {
+                JarFile jarFile = new JarFile(this.modFile);
+                Enumeration<JarEntry> enumeration = jarFile.entries();
 
-							while (is.available() > 0) {
-								fos.write(is.read());
-							}
+                while (enumeration.hasMoreElements()) {
+                    JarEntry file = (JarEntry) enumeration.nextElement();
 
-							fos.close();
-							is.close();
-							VanillaMagic.logInfo(fileName + " created.");
-							return localFile;
-						}
-					}
-				} else if (this.modFile.isDirectory()) {
-					File[] files = this.modFile.listFiles();
+                    if (file.getName().equals(fileName)) {
+                        InputStream is = jarFile.getInputStream(file);
+                        FileOutputStream fos = new FileOutputStream(localFile);
 
-					for (File file : files) {
-						if (file.getName().contains(fileName)) {
-							FileInputStream is = new FileInputStream(file);
-							FileOutputStream fos = new FileOutputStream(localFile);
+                        while (is.available() > 0) {
+                            fos.write(is.read());
+                        }
 
-							while (is.available() > 0) {
-								fos.write(is.read());
-							}
+                        fos.close();
+                        is.close();
+                        VMLogger.logInfo(fileName + " created.");
 
-							fos.close();
-							is.close();
-							VanillaMagic.logInfo(fileName + " created.");
-							return localFile;
-						}
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				VanillaMagic.logInfo("Error while creating " + fileName);
-			}
-		}
-		return localFile;
-	}
+                        return localFile;
+                    }
+                }
+            } else if (this.modFile.isDirectory()) {
+                File[] files = this.modFile.listFiles();
 
-	/**
-	 * Read all advancements from advancements.json file
-	 */
-	public void readAchievements() {
-		try {
-			FileInputStream fis = new FileInputStream(this.fileAchievements);
-			InputStreamReader reader = new InputStreamReader(fis);
-			JsonParser parser = new JsonParser();
-			JsonElement js = parser.parse(reader);
+                for (File file : files) {
+                    if (file.getName().contains(fileName)) {
+                        FileInputStream is = new FileInputStream(file);
+                        FileOutputStream fos = new FileOutputStream(localFile);
 
-			if (js.isJsonArray()) {
-				JsonArray ja = js.getAsJsonArray();
+                        while (is.available() > 0) {
+                            fos.write(is.read());
+                        }
 
-				for (JsonElement je : ja) {
-					JsonObject jo = je.getAsJsonObject();
-					Class<?> className = Class.forName(jo.get("questClass").getAsString()); // Read Quest class.
-					IQuest quest = (IQuest) className.newInstance(); // Create Quest instance
-					quest.readData(jo); // Read Quest data.
-				}
+                        fos.close();
+                        is.close();
+                        VMLogger.logInfo(fileName + " created.");
 
-				VanillaMagic.log(Level.WARN, "VanillaMagic Quests readded from JSON file.");
-			} else {
-				VanillaMagic.log(Level.WARN, "Can't load VanillaMagic Quests from JSON file.");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			VanillaMagic.logInfo("Error while loading Quests.json");
-		}
-	}
+                        return localFile;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            VMLogger.logInfo("Error while creating " + fileName);
+        }
+
+        return localFile;
+    }
+
+    /**
+     * Read all Quests from JSON file
+     */
+    public void readQuests() {
+        try {
+            FileInputStream fis = new FileInputStream(this.fileQuests);
+            InputStreamReader reader = new InputStreamReader(fis);
+            JsonParser parser = new JsonParser();
+            JsonElement js = parser.parse(reader);
+
+            if (!js.isJsonArray()) {
+                VMLogger.log(Level.WARN, "Can't load VanillaMagic Quests from JSON file.");
+                return;
+            }
+
+            JsonArray ja = js.getAsJsonArray();
+
+            for (JsonElement je : ja) {
+                JsonObject jo = je.getAsJsonObject();
+                Class<?> className = Class.forName(jo.get("questClass").getAsString());
+                IQuest quest = (IQuest) className.newInstance();
+                quest.readData(jo);
+            }
+
+            VMLogger.log(Level.WARN, "VanillaMagic Quests read from JSON file.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            VMLogger.logInfo("Error while loading Quests.json");
+        }
+    }
 }
