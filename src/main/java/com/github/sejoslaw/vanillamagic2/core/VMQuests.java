9a4a9646f243817723b43951cb06e71dde36b16e
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-07-11 20:24:50 +0200

    [VM2] Added basic VM File handling

--- src/main/java/com/github/sejoslaw/vanillamagic2/core/VMQuests.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/core/VMQuests.java
@@ -3,18 +3,12 @@ package com.github.sejoslaw.vanillamagic2.core;
 import com.github.sejoslaw.vanillamagic2.common.registries.QuestRegistry;
 import org.apache.logging.log4j.Level;
 
-import java.io.File;
-import java.nio.file.Path;
-
 /**
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
 public final class VMQuests {
     public static void initialize() {
-        Path questsConfigPath = VMFiles.getQuestsFilePath();
-        File questsConfigFile = questsConfigPath.toFile();
-
-        VMFiles.readJson(questsConfigFile, rootElement -> {
+        VMFiles.readJson(VMFiles.getQuestsFilePath().toFile(), rootElement -> {
             if (!rootElement.isJsonArray()) {
                 VMLogger.log(Level.WARN, "Can't load VanillaMagic Quests from JSON file.");
                 return;
