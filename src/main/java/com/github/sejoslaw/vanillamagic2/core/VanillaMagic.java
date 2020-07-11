bf599dc3681291b0d793b531ee3fe6a850982310
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-07-11 12:30:38 +0200

    Starting work on Vanilla Magic 2 !!!

---
+++ src/main/java/com/github/sejoslaw/vanillamagic2/core/VanillaMagic.java
@@ -0,0 +1,14 @@
+package com.github.sejoslaw.vanillamagic2.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author Sejoslaw - https://github.com/Sejoslaw
+ */
+@Mod(VanillaMagic.MODID)
+public class VanillaMagic {
+    public static final String MODID = "vanillamagic2";
+
+    public VanillaMagic() {
+    }
+}
