65090a1818c2a45708816087371754b8475a1f6a
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-07-23 16:47:53 +0200

    [VM2] Added generalized EventCallerUpgradable

--- src/main/java/com/github/sejoslaw/vanillamagic2/common/quests/eventCallers/EventCallerItemTierUpgrade.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/common/quests/eventCallers/EventCallerItemTierUpgrade.java
@@ -1,65 +1,28 @@
 package com.github.sejoslaw.vanillamagic2.common.quests.eventCallers;
 
-import com.github.sejoslaw.vanillamagic2.common.quests.EventCaller;
 import com.github.sejoslaw.vanillamagic2.common.quests.types.QuestItemTierUpgrade;
 import com.github.sejoslaw.vanillamagic2.common.registries.ItemTierRegistry;
-import com.github.sejoslaw.vanillamagic2.common.utils.WorldUtils;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
-public class EventCallerItemTierUpgrade extends EventCaller<QuestItemTierUpgrade> {
-    @SubscribeEvent
-    public void onRightClick(PlayerInteractEvent.RightClickBlock event) {
-        final List<ItemEntity>[] items = new List[1];       // All items in Cauldron
-        final List<ItemEntity>[] baseItems = new List[1];   // "Base" items, i.e. sword, pickaxe, axe, helmet, etc.
-        final List<ItemEntity>[] ingredients = new List[1]; // Ingredients required for ANY upgrade
-        final List<ItemStack>[] results = new List[1];      // Results from crafting using available se items and ingredients
-
-        this.executor.onPlayerInteract(event,
-                (player, world, pos, direction) ->
-                        this.executor.clickCauldron(world, pos, () -> {
-                            items[0] = WorldUtils.getItems(world, pos);
-                            baseItems[0] = items[0].stream().filter(entity -> ItemTierRegistry.isBase(entity.getItem())).collect(Collectors.toList());
-                            ingredients[0] = items[0].stream().filter(entity -> ItemTierRegistry.isIngredient(entity.getItem())).collect(Collectors.toList());
-
-                            if (baseItems[0].size() <= 0 || ingredients[0].size() <= 0) {
-                                return null;
-                            }
-
-                            results[0] = this.getResults(baseItems[0], ingredients[0]);
-
-                            if (results[0].size() <= 0) {
-                                return null;
-                            }
-
-                            return this.quests.get(0);
-                        }),
-                (player, world, pos, direction, quest) -> {
-                    baseItems[0].forEach(Entity::remove);
-
-                    ingredients[0].forEach(entity -> {
-                        if (entity.getItem().getCount() <= 0) {
-                            entity.remove();
-                        }
-                    });
-
-                    WorldUtils.spawnOnCauldron(world, pos, results[0], ItemStack::getCount);
-                });
+public class EventCallerItemTierUpgrade extends EventCallerUpgradable<QuestItemTierUpgrade> {
+    protected boolean isBase(ItemStack stack) {
+        return ItemTierRegistry.isBase(stack);
     }
 
-    private List<ItemStack> getResults(List<ItemEntity> baseItems, List<ItemEntity> ingredients) {
+    protected boolean isIngredient(ItemStack stack) {
+        return ItemTierRegistry.isIngredient(stack);
+    }
+
+    protected List<ItemStack> getResults(List<ItemEntity> baseItems, List<ItemEntity> ingredients) {
         List<ItemStack> results = new ArrayList<>();
 
         for (ItemEntity baseEntity : baseItems) {
