cc39d439565a7fa7618f9b08f68a70f3654118ed
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-07-24 20:22:24 +0200

    [VM2] Added more function type interfaces

---
+++ src/main/java/com/github/sejoslaw/vanillamagic2/common/functions/Consumer6.java
@@ -0,0 +1,8 @@
+package com.github.sejoslaw.vanillamagic2.common.functions;
+
+/**
+ * @author Sejoslaw - https://github.com/Sejoslaw
+ */
+public interface Consumer6<TFirst, TSecond, TThird, TFourth, TFifth, TSixth> {
+    void accept(TFirst first, TSecond second, TThird third, TFourth fourth, TFifth fifth, TSixth sixth);
+}
