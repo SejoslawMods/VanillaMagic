bdf200af4c15e38061dec1cc8e4b7d708d8a1052
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-08-05 09:13:35 +0200

    [VM2] Cleaned up VanillaMagic.java;

--- src/main/java/com/github/sejoslaw/vanillamagic2/core/VanillaMagic.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/core/VanillaMagic.java
@@ -12,7 +12,6 @@ public final class VanillaMagic {
     public VanillaMagic() {
         VMFiles.initialize();
         VMRegistries.initialize();
-//        VMEnergy.initialize();
         VMQuests.initialize();
 //        VMEvents.initialize();
     }
