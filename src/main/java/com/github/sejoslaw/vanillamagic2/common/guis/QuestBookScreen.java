184b3edfa82800b28c4760a1caf3ae05bc553724
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-08-12 06:34:04 +0200

    [VM2] Started working on OpenQuestBookHandler;

--- src/main/java/com/github/sejoslaw/vanillamagic2/common/guis/QuestBookScreen.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/common/guis/QuestBookScreen.java
@@ -3,11 +3,14 @@ package com.github.sejoslaw.vanillamagic2.common.guis;
 import com.github.sejoslaw.vanillamagic2.common.utils.TextUtils;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 /**
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
-public class QuestBookScreen extends Screen {
+@OnlyIn(Dist.CLIENT)
+public class QuestBookScreen extends Screen { // TODO: Implement !!!
     public QuestBookScreen() {
         this(TextUtils.translate("vm.gui.questBookTitle"));
     }
