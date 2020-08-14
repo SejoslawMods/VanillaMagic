5215dba569888ed1edc317c100ce1f6f4cf2dfc6
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-08-14 18:25:27 +0200

    [VM2] Started working on QuestBookScreen;

--- src/main/java/com/github/sejoslaw/vanillamagic2/common/guis/QuestBookScreen.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/common/guis/QuestBookScreen.java
@@ -10,7 +10,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
 @OnlyIn(Dist.CLIENT)
-public class QuestBookScreen extends Screen { // TODO: Implement !!!
+public class QuestBookScreen extends Screen { // TODO: Design methods and their functionality
     public QuestBookScreen() {
         this(TextUtils.translate("vm.gui.questBookTitle"));
     }
@@ -18,4 +18,33 @@ public class QuestBookScreen extends Screen { // TODO: Implement !!!
     protected QuestBookScreen(ITextComponent titleIn) {
         super(titleIn);
     }
+
+//    protected void init() {
+//        // TODO: Gather Player's Quests data to be displayed.
+//    }
+//
+//    public void removed() {
+//        // TODO: Clean gathered data.
+//    }
+//
+//    public boolean mouseClicked(double mouseX, double mouseY, int keyCode) {
+//    }
+//
+//    public boolean mouseReleased(double mouseX, double mouseY, int keyCode) {
+//    }
+//
+//    public boolean keyPressed(int keySym, int scancode, int p_keyPressed_3_) {
+//    }
+//
+//    public void render(int mouseX, int mouseY, float partialTicks) {
+//    }
+//
+//    public boolean mouseDragged(double mouseX, double mouseY, int keyCode, double deltaX, double deltaY) {
+//    }
+//
+//    public void renderBackground() {
+//        // TODO: Add rendering (whole screen) stone + ores
+//    }
+//
+//    // TODO: ContainerScreen.drawItemStack
 }
