aaa8bdf3e0d8517a06b6c21f7ee49d82f683c822
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-08-29 19:54:01 +0200

    [VM2] Cleaning;

--- src/main/java/com/github/sejoslaw/vanillamagic2/common/quests/types/QuestMobSpawnerDrop.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/common/quests/types/QuestMobSpawnerDrop.java
@@ -1,9 +1,17 @@
 package com.github.sejoslaw.vanillamagic2.common.quests.types;
 
 import com.github.sejoslaw.vanillamagic2.common.quests.Quest;
+import com.github.sejoslaw.vanillamagic2.common.utils.TextUtils;
+
+import java.util.Collection;
 
 /**
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
 public class QuestMobSpawnerDrop extends Quest {
+    public void fillTooltip(Collection<String> lines) {
+        super.fillTooltip(lines);
+
+        this.addLine(lines, "quest.tooltip.usage", TextUtils.getFormattedText("quest.mobSpawnerDropBlock.desc.usage"));
+    }
 }
