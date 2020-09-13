6f1d009642ee62fe37da1965e53e35ec3ac42039
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-09-13 08:59:38 +0200

    [VM2] Fixing Quests;

--- src/main/java/com/github/sejoslaw/vanillamagic2/common/quests/types/QuestItemUpgrade.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/common/quests/types/QuestItemUpgrade.java
@@ -1,9 +1,29 @@
 package com.github.sejoslaw.vanillamagic2.common.quests.types;
 
+import com.github.sejoslaw.vanillamagic2.common.itemupgrades.BaseItemType;
 import com.github.sejoslaw.vanillamagic2.common.quests.Quest;
+import com.github.sejoslaw.vanillamagic2.common.registries.ItemUpgradeRegistry;
+import com.github.sejoslaw.vanillamagic2.common.utils.TextUtils;
+
+import java.util.Collection;
 
 /**
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
 public class QuestItemUpgrade extends Quest {
+    public void fillTooltip(Collection<String> lines) {
+        super.fillTooltip(lines);
+
+        TextUtils.addLine(lines, "", "");
+        TextUtils.addLine(lines, "quest.tooltip.crafting", "");
+
+        BaseItemType.TYPES.forEach(baseItemType ->
+                ItemUpgradeRegistry.getUpgrades(baseItemType).forEach(itemUpgrade -> {
+                    String message =
+                            TextUtils.firstLetterToUpper(baseItemType.itemType) + " + " +
+                            this.getTooltip(itemUpgrade.getIngredient()) + " = " +
+                            itemUpgrade.getClass().getSimpleName();
+                    TextUtils.addLine(lines, "", message);
+        }));
+    }
 }
