c7b0d2aa0f8955c46f78b6b19b76e9a6602f70d2
Krzysztof Dobrzyński <kdobrzynski@pl.imshealth.com>
2020-10-13 12:37:40 +0200

    [VM2] Started working on Breeder;

--- src/main/java/com/github/sejoslaw/vanillamagic2/common/tileentities/machines/modules/breeders/UsingItemBreederLogicModule.java
+++ src/main/java/com/github/sejoslaw/vanillamagic2/common/tileentities/machines/modules/breeders/UsingItemBreederLogicModule.java
@@ -1,13 +1,19 @@
 package com.github.sejoslaw.vanillamagic2.common.tileentities.machines.modules.breeders;
 
 import com.github.sejoslaw.vanillamagic2.common.tileentities.machines.IVMTileMachine;
+import net.minecraft.entity.passive.AnimalEntity;
 
 /**
  * @author Sejoslaw - https://github.com/Sejoslaw
  */
 public class UsingItemBreederLogicModule extends AbstractBreederLogicModule {
     protected void work(IVMTileMachine machine) {
-        this.useSlot(machine, (inv, slotId, stack) -> {
-        });
+        this.useSlot(machine, (inv, slotId, stack) ->
+            this.getEntities(machine, AnimalEntity.class, entity -> true)
+                    .forEach(entity -> {
+                        if (entity.isBreedingItem(stack)) {
+                            entity.setInLove(600);
+                        }
+                    }));
     }
 }
